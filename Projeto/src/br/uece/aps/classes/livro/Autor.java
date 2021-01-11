034e1c75393f6a894c61c1d9bb3e37e48a2af3dd
JosephUchoa <jose.g.uchoa@gmail.com>
2021-01-11 17:41:04 -0300

    Enum de Autores

--- Projeto/src/br/uece/aps/classes/livro/Autor.java
+++ Projeto/src/br/uece/aps/classes/livro/Autor.java
@@ -1,15 +1,20 @@
 package br.uece.aps.classes.livro;
 
-public class Autor {
-    
-    private String Nome;
-
-    public String getNome() {
-        return Nome;
-    }
-
-    public void setNome(String Nome) {
-        this.Nome = Nome;
-    }
-    
+public enum Autor {
+	CLARICE("Clarice Lispector"),
+	ASSIS("Machado de Assis"),
+	KAFKA("Franz Kafka"),
+	DRUMMOND("Carlos Drummond"),
+	SHAKESPEARE("William Shakespeare"),
+	ALENCAR("José de Alencar"),
+	ANDRZEJ("Andrzej Sapkowski");
+	
+	private final String autor;
+	Autor(String autor) {
+		this.autor = autor;
+	}
+	
+	public String getAutor() {
+		return this.autor;
+	}
 }
