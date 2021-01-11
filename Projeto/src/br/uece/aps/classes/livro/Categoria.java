a84a918097bad9b223577b361ef27487197a3611
JosephUchoa <jose.g.uchoa@gmail.com>
2021-01-11 17:41:17 -0300

    Enum Categorias

--- Projeto/src/br/uece/aps/classes/livro/Categoria.java
+++ Projeto/src/br/uece/aps/classes/livro/Categoria.java
@@ -1,21 +1,21 @@
 package br.uece.aps.classes.livro;
 
-public class Categoria {
-    
-    private String Nome;
-    
-    public Categoria(String Nome){
-        setNome(Nome);
-    }
-
-    public String getNome() {
-        return Nome;
-    }
-
-    public void setNome(String Nome) {
-        this.Nome = Nome;
-    }
-    
-    
-    
+public enum Categoria {
+	AUTOBIOGRAFIA("autobiografia"),
+	BIOGRAFIA("biografia"),
+	FANTASIA("fantasia"),
+	FICCAOCIENTIFICA("ficção-cientifica"),
+	HORROR("horror"),
+	ROMANCE("romance"),
+	AUTOAJUDA("autoajuda"),
+	RECEITA("receita");
+	
+	private final String categoria;
+	Categoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getCategoria() {
+		return this.categoria;
+	}	
 }
