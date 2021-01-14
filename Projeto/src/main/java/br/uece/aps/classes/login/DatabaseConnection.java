414d951cf959cf1ed51c3146de607d243489ec41
JosephUchoa <jose.g.uchoa@gmail.com>
2021-01-14 15:29:47 -0300

    Criação de tabelas

--- Projeto/src/main/java/br/uece/aps/classes/login/DatabaseConnection.java
+++ Projeto/src/main/java/br/uece/aps/classes/login/DatabaseConnection.java
@@ -1,50 +1,88 @@
 package br.uece.aps.classes.login;
 
 import java.sql.*;
+import br.uece.aps.classes.livro.Categoria;
+import br.uece.aps.classes.livro.Autor;
 
 public final class DatabaseConnection {
     
-    public void conectar(){
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:BancoDeDados.db")) {
-
-            System.out.println("Conexão realizada !!!!");
-            
-            Statement statement = connection.createStatement();
-            // criando uma tabela para usuários
-            statement.execute("CREATE TABLE IF NOT EXISTS Contas(\n" +
-            " \"cpf\" integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
-            " \"nome\" text,\n" +
-            " \"idade\" integer,\n" +
-            " \"senha\" text,\n" +
-            " \"email\" text,\n" +
-            " \"endereco\" text,\n" +
-            " \"admin\" integer DEFAULT 0\n" +
-            ");");
-            
-            // criando uma tabela para livros
-            statement.execute("CREATE TABLE IF NOT EXISTS Livros(\n" +
-            " titulo text,\n" +
-            " preco real,\n" +
-            " avaliacao real,\n" +
-            " qntavaliacoes integer DEFAULT 0,\n" +
-            " editora text\n" +
-            ");");
-            
-            // criando uma tabela para autores
-            
-            // criando uma tabela para categoria
-            
-            // criando uma tabela para editora
-
+	private Connection connection;
+	
+	
+	public DatabaseConnection() {
+		try (Connection connection = DriverManager.getConnection("jdbc:sqlite:BancoDeDados.db")) {
+			System.out.println("Conexão realizada.");
+			this.connection = connection;	
+			
+			createTableContas();
+			System.out.println("ok");
+			createTableLivroS();
+			System.out.println("ok");
+			createTableCategorias();
+			System.out.println("ok");
+			createTableAutores();
+			
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        
-        //cadastrar(2, "Michael", 1, "123", "michael.souza@aluno.uece.br", "Rua 1 Casa 1", 1);
-        //login(2, "123");
-        //login(2, "1234");
-    }
+	}
+	
+	private void createTableContas()  throws SQLException {
+    	Statement statement = this.connection.createStatement();
+	    statement.execute("CREATE TABLE IF NOT EXISTS Contas(\n" +
+	    " \"cpf\" integer NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+	    " \"nome\" text,\n" +
+	    " \"idade\" integer,\n" +
+	    " \"senha\" text,\n" +
+	    " \"email\" text,\n" +
+	    " \"endereco\" text,\n" +
+	    " \"admin\" integer DEFAULT 0\n" +
+	    ");");
+	}
     
+	private void createTableLivroS()  throws SQLException {
+		Statement statement = this.connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS Livros(\n" +
+        " titulo text,\n" +
+        " preco real,\n" +
+        " avaliacao real,\n" +
+        " qntavaliacoes integer DEFAULT 0,\n" +
+        " editora text\n" +
+        ");");		
+	}
+	
+    private void createTableCategorias() throws SQLException {       
+    	int count = Categoria.values().length;     
+    	String sql = "CREATE TABLE IF NOT EXISTS Categorias(\n";
+    	for(Categoria categoria : Categoria.values()) {
+    		if(--count == 0) {
+    			sql += categoria.name() + " text\n";
+    		} else {
+    			sql += categoria.name() + " text,\n";
+    		}
+    	}
+    	sql += ");";
+    	
+    	Statement statement = this.connection.createStatement();
+    	statement.execute(sql.toString());
+	}
+	
+	private void createTableAutores()  throws SQLException {    	
+		int count = Autor.values().length;     
+		String sql = "CREATE TABLE IF NOT EXISTS Categorias(\n";
+		for(Autor autor : Autor.values()) {
+			if(--count == 0) {
+				sql += autor.name() + " text\n";
+			} else {
+				sql += autor.name() + " text,\n";
+			}
+		}
+		sql += ");";
+		
+		Statement statement = this.connection.createStatement();
+		statement.execute(sql.toString());
+	}
+
     public void cadastrar(Integer CPF, String Nome, Integer Idade, String Senha, String Email, String Endereco, Integer Admin){
         
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:BancoDeDados.db")) {
@@ -95,10 +133,6 @@ public final class DatabaseConnection {
             System.out.println(e.getMessage());
         }
         
-    }
-    
-    public DatabaseConnection(){
-        conectar();
     }
     
 }
