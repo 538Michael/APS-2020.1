ac2290afe3c1355bfea2848a659b9f6572715356
JosephUchoa <jose.g.uchoa@gmail.com>
2021-01-19 15:23:42 -0300

    Pagina carrinho de compras
    Não faz nada, mas abre

---
+++ livraria_uece/lib/pages/shoppingcartPage.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ShoppingCartPage extends StatefulWidget {
+  @override
+  _ShoppingCartState createState() => _ShoppingCartState();
+}
+
+class _ShoppingCartState extends State<ShoppingCartPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carrinho"),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+          child: Container(
+            padding: EdgeInsets.only(top: 20, left: 40, right: 40),
+          )
+
+      )
+    );
+  }
+}
